62a09adadf97f0bb3775cdf8366253a674dc93d9
Jorge Rodríguez Sánchez <jorosadev@gmail.com>
2023-09-04 15:53:43 +0100

    up

---
+++ tests/apps/mooc/backend/features/courses/create-product.feature
@@ -0,0 +1,4 @@
+Feature: Create a new product
+  In order to have product in the platform
+  As a user with admin permissions
+  I want to create a new product
--- tests/apps/mooc/backend/features/step_definitions/controller.steps.ts
+++ tests/apps/mooc/backend/features/step_definitions/controller.steps.ts
@@ -1,4 +1,3 @@
-import assert from 'assert';
 import { AfterAll, BeforeAll, Given, Then } from 'cucumber';
 import request from 'supertest';
 
@@ -6,31 +5,20 @@ import { MoocBackendApp } from '../../../../../../src/apps/mooc/backend/MoocBack
 
 let _request: request.Test;
 let application: MoocBackendApp;
-let _response: request.Response;
 
 Given('I send a GET request to {string}', (route: string) => {
 	_request = request(application.httpServer).get(route);
 });
 
 Then('the response status code should be {int}', async (status: number) => {
-	_response = await _request.expect(status);
-});
-
-Given('I send a PUT request to {string} with body:', (route: string, body: string) => {
-	_request = request(application.httpServer)
-		.put(route)
-		.send(JSON.parse(body) as object);
-});
-
-Then('the response should be empty', () => {
-	assert.deepStrictEqual(_response.body, {});
+	await _request.expect(status);
 });
 
 BeforeAll(() => {
 	application = new MoocBackendApp();
-	application.start();
+	void application.start();
 });
 
 AfterAll(() => {
-	application.stop();
+	void application.stop();
 });
